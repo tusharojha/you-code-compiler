b0d98808b6538b3813ba6d75bdb763133ac45d7d
tusharojha <tusharojha2001@gmail.com>
2020-04-26 03:05:31 +0530

    basic AceEditor added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,41 @@
-import React from "react";
+// Library Imports
+import React, { useState } from "react";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-monokai";
+
 
 const App = () => {
+  const [code, setCode] = useState("")
+  const onLoad = ()=>{
+    console.log("loaded")
+  };
+  const onChange = (data)=>{
+    setCode(data)
+  }
   return (
     <div>
-      <h4>Welcome to React</h4>
+      <AceEditor
+        placeholder="Code goes here"
+        mode="java"
+        theme="monokai"
+        name="editor"
+        onLoad={onLoad}
+        onChange={onChange}
+        fontSize={14}
+        value={code}
+        showPrintMargin={true}
+        showGutter={true}
+        highlightActiveLine={true}
+        setOptions={{
+          enableBasicAutocompletion: false,
+          enableLiveAutocompletion: true,
+          enableSnippets: false,
+          showLineNumbers: true,
+          tabSize: 2,
+        }}
+      />
     </div>
   );
 };
