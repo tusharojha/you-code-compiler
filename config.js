2eb4f46b5018733e8fdd0ae2666e3172a9d9e4f1
tusharojha <tusharojha2001@gmail.com>
2020-04-27 16:09:52 +0530

    project deployed

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    clientId: "85e6f686d30528093af869efc1369684",
-    clientSecret:"ab46f54396772dee1b18b02f4dd93c206d734b12a534b59c0c4b3dda42f900fc"
+    clientId: "<ENTER_YOUR_CLIENT_ID>",
+    clientSecret:"<ENTER_YOUR_CLIENT_SECRET>"
 }
